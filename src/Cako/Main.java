16c60c74fddedb1be8fb3c50300b31a5c89b9e84
Nessuno <cia20003@byui.edu>
2022-01-29 12:35:04 +0100

    Initial commit

---
+++ src/Cako/Main.java
@@ -0,0 +1,15 @@
+package Cako;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    sayHello();
+    }
+
+    static void sayHello() {
+        System.out.println("Hello, my name is Denis.");
+    }
+
+}
