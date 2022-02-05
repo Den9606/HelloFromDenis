6fb737ef23ff57dee8c80933d524b51b19f25f9a
JHowe <how19003@byui.edu>
2022-02-05 09:52:28 -0700

    Josiah's edits

--- src/Cako/Main.java
+++ src/Cako/Main.java
@@ -6,10 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
 	    sayHello();
+        helloAgain();
     }
 
     static void sayHello() {
         System.out.println("Hello, my name is Denis.");
+    }
+    static void helloAgain() {
+        System.out.println("Hello Denis, my name is Josiah.");
     }
 
 }
